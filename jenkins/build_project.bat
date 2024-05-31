8f4e3d430af9ef3e171583f71db8ef6cd0854d62
kevin.audrain <kevin.audrain@hii-tsd.com>
2024-05-31 11:58:23 -0400

    Tic-04 Changed the path to project since bats are run from root

--- jenkins/build_project.bat
+++ jenkins/build_project.bat
@@ -5,7 +5,7 @@ REM Path to your Unity executable
 set UNITY_PATH="C:\Program Files\Unity\Hub\Editor\2022.3.31f1\Editor\Unity.exe"
 
 REM Path to your Unity project
-set PROJECT_PATH="C:\Users\kevin\source\repos\TicTacToe\TicTacToe"
+set PROJECT_PATH="TicTacToe"
 
 REM Name of the method to execute
 set METHOD_NAME="BuildScript.BuildProject"
--- jenkins/run_editmode_tests.bat
+++ jenkins/run_editmode_tests.bat
@@ -5,7 +5,7 @@ REM Path to your Unity executable
 set UNITY_PATH="C:\Program Files\Unity\Hub\Editor\2022.3.31f1\Editor\Unity.exe"
 
 REM Path to your Unity project
-set PROJECT_PATH="..\TicTacToe"
+set PROJECT_PATH="TicTacToe"
 
 REM Path to save the test results
 set RESULTS_PATH="C:\TestResults"
